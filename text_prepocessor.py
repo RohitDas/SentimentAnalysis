d1aa59dd5aa712e65635e9922753f8e87b26abd0
rohit <rohitangsudas91@gmail.com>
2018-08-31 03:25:47 +0800

    Text preprocessor coded

--- text_prepocessor.py
+++ text_prepocessor.py
@@ -1,6 +1,6 @@
 import nltk
-import nltk.corpus import stopwords
-import nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import string
 
 class Textpreprocessor(object):
@@ -14,7 +14,7 @@ class Textpreprocessor(object):
             nltk.download("stopwords")
             self.stop_words = set(stopwords.words("english"))
 
-    def tokenize(self, text)
+    def tokenize(self, text):
         return text.split(" ")
     
     def convert_lower_case(self, text):
@@ -24,7 +24,7 @@ class Textpreprocessor(object):
         return text.translate(None, string.punctuation)
 
     def ignore_stopwords(self, token_list):
-        return [w for w in tokens if not w in stop_words]
+        return [w for w in token_list if not w in self.stop_words]
 
     def fix_misspelled_words(self, token_list):
         return None
@@ -32,15 +32,31 @@ class Textpreprocessor(object):
     def stemming(self, text):
         return None
 
+    def auxillary_preprocess(self, text_with_rating):
+        func_list = [self.convert_lower_case, 
+                     self.ignore_punctuation,
+                     self.tokenize,
+                     self.ignore_stopwords]
+        inp = text_with_rating[0]
+        for func in func_list:
+            inp = func(inp)
+
+        return (inp, text_with_rating[1])
+
     def process_text(self, 
                      text_generator, 
                      correct_spell=False,
                      stem=False):
         for batch in text_generator:
-            #convert to lower case
-            #ignore punctuation
-            #remove stop_words
-            #fix misspelled words
-            #Stemming
-            pass
+            yield map(self.auxillary_preprocess, batch)
+            
+
+if __name__ == "__main__":
+    from input_reader import InputReader
+    input_reader = InputReader(10)
+    input_dir = "/home/rohittulu/Documents/aclImdb/train/pos/"
+    text_generator = input_reader.get_batches(input_dir)
+    text_processor = Textpreprocessor()
+    for processed_batch in text_processor.process_text(text_generator):
+        print processed_batch
 
